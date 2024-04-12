76294a9841f5c8edb7a0768ea4fde7dd9d6c72c0
Bruno Sanchez <bsanchez@cppm.in2p3.fr>
2024-04-12 22:50:31 +0200

    Re-organizing content and changing theme.

--- docs/conf.py
+++ docs/conf.py
@@ -91,13 +91,13 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#html_theme = 'default'
-
-# import zerovm_sphinx_theme
-# html_theme = 'zerovm'
-# html_theme_path = [zerovm_sphinx_theme.theme_path]
-
+html_theme = 'default'
 html_theme = 'groundwork'
+
+import zerovm_sphinx_theme
+html_theme = 'zerovm'
+html_theme_path = [zerovm_sphinx_theme.theme_path]
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
